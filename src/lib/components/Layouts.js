4d2d7f5aaf94d0536cdd29f389379db6d48dcb25
Francisco Hodge <hello@franciscohodge.com>
2018-11-14 09:28:20 -0500

    Adding italian language

--- src/lib/components/Layouts.js
+++ src/lib/components/Layouts.js
@@ -1,15 +1,17 @@
 import arabic from '../layouts/arabic';
 import chinese from'../layouts/chinese';
+import english from'../layouts/english';
 import french from '../layouts/french';
 import german from '../layouts/german';
 import hindi from '../layouts/hindi';
+import italian from '../layouts/italian';
 import japanese from '../layouts/japanese';
 import korean from '../layouts/korean';
 import russian from '../layouts/russian';
+import sindhi from '../layouts/sindhi';
 import spanish from '../layouts/spanish';
 import thai from '../layouts/thai';
 import urdu from '../layouts/urdu';
-import sindhi from '../layouts/sindhi';
 
 class SimpleKeyboardLayouts {
 
@@ -17,16 +19,18 @@ class SimpleKeyboardLayouts {
     const layouts = {
       arabic,
       chinese,
+      english,
       french,
       german,
       hindi,
+      italian,
       japanese,
       korean,
       russian,
+      sindhi,
       spanish,
       thai,
-      urdu,
-      sindhi
+      urdu
     };
 
     return layouts[layout];
