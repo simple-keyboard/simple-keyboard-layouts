daa508afcdf959d2ff4223310b49e5be192f22f7
Francisco Hodge <hello@franciscohodge.com>
2019-01-06 16:09:47 -0500

    Updated devDependencies

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-layouts v1.2.2
+ *   simple-keyboard-layouts v1.2.3
  *   https://github.com/hodgef/simple-keyboard-layouts
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
