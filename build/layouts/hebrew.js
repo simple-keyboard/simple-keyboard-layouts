6efaf944f3444c155b87ef89988a5e36bfac98e2
Francisco Hodge <hello@franciscohodge.com>
2019-08-30 14:50:43 -0400

    npm update

---
+++ build/layouts/hebrew.js
@@ -0,0 +1,1 @@
+const hebrew={default:["` 1 2 3 4 5 6 7 8 9 0 - = {bksp}","{tab} / ' ק ר א ט ו ן ם פ ] [ \\","{lock} ש ד ג כ ע י ח ל ך ף , {enter}","{shift} ז ס ב ה נ מ צ ת ץ . {shift}",".com @ {space}"],shift:["~ ! @ # $ % ^ & * ( ) _ + {bksp}","{tab} Q W E R T Y U I O P { } |",'{lock} A S D F G H J K L : " {enter}',"{shift} Z X C V B N M < > ? {shift}",".com @ {space}"]};export default hebrew;
