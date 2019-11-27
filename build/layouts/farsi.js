0e1224f4bbdad079960aa8b9d0bfc7bf5f44eee5
Francisco Hodge <hello@franciscohodge.com>
2019-11-27 01:21:56 +0000

    Build update

---
+++ build/layouts/farsi.js
@@ -0,0 +1,1 @@
+const farsi={default:["` ۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ ۰ - = {bksp}","{tab} ض ص ث ق ف غ ع ه خ ح ج چ \\","{lock} ش س ی ب ل ا ت ن م ک گ {enter}","{shift} ظ ط ز ر ذ د پ و . / {shift}",".com @ {space}"],shift:["÷ ! ٬ ٫ ﷼ ٪ × ۷ * ) ( ـ + {bksp}","{tab} ْ ٌ ٍ ً ُ ِ َ ّ ] [ } {","{lock} ؤ ئ ي إ أ آ ة » « : ؛ {enter}","{shift} ك ٓ ژ ٰ ‌ ٔ ء < > ؟ {shift}",".com @ {space}"]};export default farsi;
