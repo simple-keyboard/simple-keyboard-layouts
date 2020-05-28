02e1b8b0bd20e48599ee12238ff834c09731e119
Francisco Hodge <hello@franciscohodge.com>
2020-05-28 10:32:53 -0400

    Update polish.js

--- src/lib/layouts/polish.js
+++ src/lib/layouts/polish.js
@@ -1,18 +1,18 @@
 const polish = {
     default: [
-        '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
-        '{tab} q w e r t y u i o p [ ] \\',
-        '{lock} a s d f g h j k l ; \' {enter}',
-        '{shift} z x c v b n m , . / {shift}',
-        '.com @ {space} ą ę ć ł ń ó ś ż ź'
+      "\u02DB 1 2 3 4 5 6 7 8 9 0 + ' {bksp}",
+      "{tab} q w e r t z u i o p \u017C \u015B",
+      "{lock} a s d f g h j k l \u0142 \u0105 \u00F3 {enter}",
+      "{shift} < y x c v b n m , . - {shift}",
+      ".com @ {space}"
     ],
     shift: [
-        '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
-        '{tab} Q W E R T Y U I O P { } |',
-        '{lock} A S D F G H J K L : " {enter}',
-        '{shift} Z X C V B N M < > ? {shift}',
-        '.com @ {space} Ą Ę Ć Ł Ń Ó Ś Ż Ź'
-    ]
+      "\u00B7 ! \" # \u00A4 % & / ( ) = ? * {bksp}",
+      "{tab} Q W E R T Z U I O P \u0144 \u0107",
+      "{lock} A S D F G H J K L \u0141 \u0119 \u017A {enter}",
+      "{shift} > Y X C V B N M ; : _ {shift}",
+      ".com @ {space}"
+    ],
 };
 
 export default polish;
