8473187d513b91c14a6c400fa549281519d81102
maciej.sielski <maciej.sielski@pl.abb.com>
2020-05-28 15:57:32 +0200

    added polish and greek layouts

---
+++ src/lib/layouts/greek.js
@@ -0,0 +1,23 @@
+export const greek = {
+    default: [
+        '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+        '{tab} ; ς ε ρ τ υ θ ι ο π [ ] \\',
+        '{lock} α σ δ φ γ η ξ κ λ ΄ \' {enter}',
+        '{shift} < ζ χ ψ ω β ν μ , . / {shift}',
+        '.com @ {space} ΅ ΄ · {extras}'
+    ],
+    shift: [
+        '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+        ': ΅ Ε Ρ Τ Υ Θ Ι Ο Π { } |',
+        '{lock} Α Σ Δ Φ Γ Η Ξ Κ Λ ¨ " {enter}',
+        '{shift} > Ζ Χ Ψ Ω Β Ν Μ < > ? {shift}',
+        '.com @ {space} ΅ ΄ · {extras}'
+    ],
+    extras: [
+        '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+        'ά έ ί ύ ό ώ',
+        'Ά Ώ Έ Ί Ύ Ό',
+        'ϊ ϋ ΅ ΄ · ΐ ΰ',
+        '{abc}'
+    ]
+};
