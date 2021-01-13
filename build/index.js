1666e2414a06c97cbe18830e05667717efc211b3
Francisco Hodge <hello@franciscohodge.com>
2021-01-13 08:00:41 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-layouts v1.15.95
+ *   simple-keyboard-layouts v1.15.96
  *   https://github.com/hodgef/simple-keyboard-layouts
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
