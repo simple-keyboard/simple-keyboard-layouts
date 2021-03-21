97babbd094a4f798617d620f5c49884e9fc070e0
Francisco Hodge <hello@franciscohodge.com>
2021-03-21 19:45:27 +0000

    Build update

--- build/layouts/chinese.js
+++ build/layouts/chinese.js
@@ -14,7 +14,7 @@ export default {
         shift: [
             "~ ! @ # $ % ^ & * ) ( _ + {bksp}",
             "{tab} Q W E R T Y U I O P { } |",
-            "{lock} A S D F G H J K L : {enter}",
+            '{lock} A S D F G H J K L : " {enter}',
             "{shift} Z X C V B N M < > ? {shift}",
             ".com @ {space}",
         ],
