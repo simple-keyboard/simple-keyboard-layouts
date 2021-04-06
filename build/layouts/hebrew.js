ce4088166ec9e4e4ce893e228b77b020b6e04117
Daniel Liverant <daniel.liverant@justeattakeaway.com>
2021-04-06 16:14:02 +0300

    fix(hebrew): align keys with physical Hebrew keyboard

--- build/layouts/hebrew.js
+++ build/layouts/hebrew.js
@@ -5,10 +5,10 @@
 export default {
     layout: {
         default: [
-            "\u05e5 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-            "{tab} \u05e3 \u05df \u05e7 \u05e8 \u05d0 \u05d8 \u05d5 \u05ea \u05dd \u05e4 ] [ \\",
-            "{lock} \u05e9 \u05d3 \u05d2 \u05db \u05e2 \u05d9 \u05d7 \u05dc \u05da : ' {enter}",
-            "{shift} \u05d6 \u05e1 \u05d1 \u05d4 \u05e0 \u05de \u05e6 , . / {shift}",
+            " 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+            "{tab} / \' \u05e7 \u05e8 \u05d0 \u05d8 \u05d5 \u05df \u05dd \u05e4 ] [ :",
+            "{lock} \u05e9 \u05d3 \u05d2 \u05db \u05e2 \u05d9 \u05d7 \u05dc \u05da \u05e3 , {enter}",
+            "{shift} \u05d6 \u05e1 \u05d1 \u05d4 \u05e0 \u05de \u05e6 \u05ea \u05e5 . {shift}",
             ".com @ {space}",
         ],
         shift: [
