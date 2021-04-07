bbef9976a55621ba5cd7eec7d3989d1cacae65e1
Francisco Hodge <hello@franciscohodge.com>
2021-04-07 20:00:30 +0000

    Build update

--- src/lib/layouts/hebrew.ts
+++ src/lib/layouts/hebrew.ts
@@ -8,10 +8,10 @@ export default <LayoutItem>{
   layout: {
     default: [
       " 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-      "{tab} / \' \u05e7 \u05e8 \u05d0 \u05d8 \u05d5 \u05df \u05dd \u05e4 ] [ :",
+      "{tab} / ' \u05e7 \u05e8 \u05d0 \u05d8 \u05d5 \u05df \u05dd \u05e4 ] [ :",
       "{lock} \u05e9 \u05d3 \u05d2 \u05db \u05e2 \u05d9 \u05d7 \u05dc \u05da \u05e3 , {enter}",
       "{shift} \u05d6 \u05e1 \u05d1 \u05d4 \u05e0 \u05de \u05e6 \u05ea \u05e5 . {shift}",
-      ".com @ {space}"
+      ".com @ {space}",
     ],
     shift: [
       "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
