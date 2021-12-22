75802608159b97a7cfcd8966ccb5a2d388bed288
d-ashokkumar <67649950+d-ashokkumar@users.noreply.github.com>
2021-12-22 22:02:13 +0530

    Added support for tamil layout
    Added support for tamil layout keyboard

---
+++ src/lib/layouts/tamil.ts
@@ -0,0 +1,26 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: Tamil
+ * Source: yogishp (https://github.com/yogishp)
+ */
+export default <LayoutItem>{
+  layout: {
+    default: [
+      "1 2 3 4 5 6 7 8 9 0 -",
+      "{tab} \u0b85 \u0b86 \u0b87 \u0b88 \u0b89 \u0b8A \u0b8E \u0b8F \u0b90 \u0b92 \u0b93 \u0b94",
+      "\u0BCD \u0BBE \u0BBF \u0BC0 \u0BC1 \u0BC2 \u0BC6 \u0BC7 \u0BC8 \u0BCA \u0BCB {bksp}",
+      "\u0b95 \u0b99 \u0b9A \u0b9E \u0b9F \u0bA3 \u0bA4 \u0bA8 \u0bAA \u0bAE {enter}",
+      "{shift} \u0bAF \u0bB0 \u0bB2 \u0bB5 \u0BB4 \u0BB3 \u0bb1 \u0bA9 , . / {shift}",
+      "@ {space}",
+    ],
+    shift: [
+      "! @ # $ % ^ & * ( ) { } [ ] | < >",
+      "{tab} \u0b85 \u0b86 \u0b87 \u0b88 \u0b89 \u0b8A \u0b8E \u0b8F \u0b90 \u0b92 \u0b93 \u0b94",
+      "\u0BCD \u0BBE \u0BBF \u0BC0 \u0BC1 \u0BC2 \u0BC6 \u0BC7 \u0BC8 \u0BCA \u0BCB {bksp}",
+      "\u0b95 \u0b99 \u0b9A \u0b9E \u0b9F \u0bA3 \u0bA4 \u0bA8 \u0bAA \u0bAE {enter}",
+      "{shift} \u0bAF \u0bB0 \u0bB2 \u0bB5 \u0BB4 \u0BB3 \u0bb1 \u0bA9 , . / {shift}",
+      "@ {space}",
+    ],
+  },
+};
