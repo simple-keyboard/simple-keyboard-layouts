ab61e03bce880399282966e8506268ffd417822f
Francisco Hodge <hello@franciscohodge.com>
2022-02-09 06:46:39 +0000

    Build update

---
+++ build/layouts/hungarian.d.ts
@@ -0,0 +1,7 @@
+import { LayoutItem } from "../interfaces";
+declare const _default: LayoutItem;
+/**
+ * Layout: Hungarian
+ * Source: Farquaad (https://github.com/Farquaad)
+ */
+export default _default;
---
+++ build/layouts/hungarian.js
@@ -0,0 +1,22 @@
+/**
+ * Layout: Hungarian
+ * Source: Farquaad (https://github.com/Farquaad)
+ */
+export default {
+    layout: {
+        default: [
+            "0 1 2 3 4 5 6 7 8 9 ö ü ó {bksp}",
+            "{tab} q w e r t z u i o p ő ú",
+            "{lock} a s d f g h j k l é á ű {enter}",
+            "{shift} í y x c v b n m , . - {shift}",
+            ".com @ {space}",
+        ],
+        shift: [
+            "§ ' \" + ! % / = ( ) Ö Ü Ó {bksp}",
+            "{tab} Q W E R T Z U I O P Ő Ú",
+            "{lock} A S D F G H J K L É Á Ű {enter}",
+            "{shift} Í Y X C V B N M ? : _ {shift}",
+            ".com @ {space}",
+        ],
+    },
+};
