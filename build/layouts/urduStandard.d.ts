364f4e4526bb6438049cd527efba0a4fec4738ce
Francisco Hodge <hello@franciscohodge.com>
2022-11-13 21:03:12 +0000

    Build update

---
+++ build/layouts/urduStandard.d.ts
@@ -0,0 +1,7 @@
+import { LayoutItem } from "../interfaces";
+/**
+ * Layout: Urdu
+ * Source: Umara Iftikhar (https://github.com/imaneumma)
+ */
+declare const _default: LayoutItem;
+export default _default;
---
+++ build/layouts/urduStandard.js
@@ -0,0 +1,22 @@
+/**
+ * Layout: Urdu
+ * Source: Umara Iftikhar (https://github.com/imaneumma)
+ */
+export default {
+    layout: {
+        default: [
+            "` \u0661 \u0662 \u0663 \u0664 \u0665 \u0666 \u0667 \u0668 \u0669 \u0660 - = {bksp}",
+            "{tab} \u0637 \u0635 \u06BE \u062F \u0679 \u067E \u062A \u0628 \u062C \u062D ] [ \u005C",
+            "{lock} \u0645 \u0648 \u0631 \u0646 \u0644 \u06C1 \u0627 \u06A9 \u0649 \u061B \u066C {enter}",
+            "{shift} \u0642 \u0641 \u06D2 \u0633 \u0634 \u063A \u0639 \u060C \u06D4 / {shift}",
+            ".com @ {space}",
+        ],
+        shift: [
+            "~ ! @ # $ \u066A ^ \u06D6 * ) ( - + {bksp}",
+            "{tab} \u0638 \u0636 \u0630 \u0688 \u062B \u0303 \u06C3 \u0640 \u0686 \u062E } { |",
+            "{lock} \u0698 \u0632 \u0691 \u06BA \u06C2 \u0621 \u0622 \u06AF \u064A \u0703 \u05f4 {enter}",
+            "{shift} \u06D3 \u0624 \u0626     > < \u061F {shift}",
+            ".com @ {space}",
+        ],
+    },
+};
