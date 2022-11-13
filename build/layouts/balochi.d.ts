579cdc0656bd33b46442375d4394c1af6244fe3f
Francisco Hodge <hello@franciscohodge.com>
2022-11-13 21:27:08 +0000

    Build update

---
+++ build/layouts/balochi.d.ts
@@ -0,0 +1,7 @@
+import { LayoutItem } from "../interfaces";
+/**
+ * Layout: Balochi
+ * Source: umairayub79 (https://github.com/umairayub79)
+ */
+declare const _default: LayoutItem;
+export default _default;
---
+++ build/layouts/balochi.js
@@ -0,0 +1,22 @@
+/**
+ * Layout: Balochi
+ * Source: umairayub79 (https://github.com/umairayub79)
+ */
+export default {
+    layout: {
+        default: [
+            "\u064B \u0661 \u0662 \u0663 \u06F4 \u06F5 \u06f6 \u06f7 \u06f8 \u06f9 \u06f0 - = {bksp}",
+            "{tab} \u0642 \u0648 \u0639 \u0631 \u062A \u06D2 \u0621 \u0649 \u06C1 \u067E [ ] \\",
+            "{lock} \u0627 \u0633 \u062F \u0641 \u06AF \u06BE \u062C \u06A9 \u0644 \u061B \u06C2 {enter}",
+            "{shift} \u0632 \u0634 \u0686 \u0637 \u0628 \u0646 \u0645 \u060C \u06D4 \u0652 {shift}",
+            ".com @ {space}",
+        ],
+        shift: [
+            "~ ! @ # $ \u066A } { * ( ) _ \u0651 + {bksp}",
+            "{tab} \u0621\u064F \u0624 \u0621\u0650 \u0691 \u0679 \u0626\u06D2 \u0670 \u0626 \u0652 \u064F \u201C \u201D",
+            "{lock} \u0622 \u0635 \u0688 \u0621\u064E \u063A \u062D \u0636 \u062E \u0626\u0650\u06D2 : \u2018 {enter}",
+            "{shift} \u0630 \u0698 \u062B \u0638 \u06CC\u0654 \u06BA \u0627\u064E\u0646\u062A \u0650 \u064E \u061F {shift}",
+            ".com @ {space}",
+        ]
+    },
+};
