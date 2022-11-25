e1cba32c072e04ce8874b48925eab03f5c20f777
GoRaN <gorangharib.909@gmail.com>
2022-11-25 22:53:07 +0300

    Create kurdish.ts

---
+++ src/lib/layouts/kurdish.ts
@@ -0,0 +1,24 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: Kurdish (کوردی)
+ * Source: Goran Gharib (https://github.com/GoRaN909)
+ */
+export default <LayoutItem>{
+  layout: {
+default: [
+      "\u0661 \u0662 \u0663 \u0664 \u0665 \u0666 \u0667 \u0668 \u0669 \u0660 - = {bksp}",
+      "{tab} \u0642 \u0648 \u06d5 \u0631 \u062a \u06cc \u06ce \u0626 \u06c6 \u067e [ ]",
+      "{lock} \u0627 \u0633 \u0634 \u062f \u0641 \u0647 \u0698 \u0644 \u06a9 \u06af {enter}",
+      "{shift} \u0632 \u062e \u062c \u0686 \u062d \u0639 \u0628 \u0646 \u0645 {shift}",
+      ".com @ {space}",
+    ],
+    shift: [
+      "~ ! @ # $ % ^ & * ) ( _ + {bksp}",
+      "{tab} \u066f \u0648\u0648 \u0629 \u0695 \u0637 \u064a \u063d \u0621 \u0639 \u0624 \u062b < > \\",
+      '{lock} \u0622 \u0635 \u0636 \u0630 \u06a4 \u06be \u0640 \u06b5 \u0643 \u063a \u061b \u003a " {enter}',
+      "{shift} | \u0638 \ufdf2 \ufdfb \ufe8b \ufdfd \ufdf4 \ufdfa \u060c \u002e \u061f {shift}",
+      ".com @ {space}",
+    ],
+  },
+};
