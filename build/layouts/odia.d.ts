b9e3f4aadc3d16e9cbc276a5585149a1efcb299a
Francisco Hodge <hello@franciscohodge.com>
2023-01-29 17:58:52 +0000

    Build update

---
+++ build/layouts/odia.d.ts
@@ -0,0 +1,7 @@
+import { LayoutItem } from "../interfaces";
+/**
+ * Layout: Odia
+ * Reference: https://learn.microsoft.com/en-us/globalization/keyboards/kbdinori.html
+ */
+declare const _default: LayoutItem;
+export default _default;
