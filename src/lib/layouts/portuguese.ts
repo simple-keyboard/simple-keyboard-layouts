847e24a98d6e7deab9f6031e4ec86a186264daee
Pradeep Teja <Pradeep.kathi@aspiresys.com>
2023-03-29 13:28:55 +0530

    Added Portuguese layout

---
+++ src/lib/layouts/portuguese.ts
@@ -0,0 +1,23 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: Portuguese
+ */
+export default <LayoutItem>{
+  layout: {
+    default: [
+      "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+      "{tab} q w e r t y u i o p \u0301 +",
+      "{lock} a s d f g h j k l \u00e7 \u00b4 {enter}",
+      "{shift} z x c v b n m , . ; {shift}",
+      ".com @ {space}",
+    ],
+    shift: [
+      "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+      "{tab} Q W E R T Y U I O P \u0308 \u0303",
+      '{lock} A S D F G H J K L \u00c7 \u00b0 {enter}',
+      "{shift} Z X C V B N M < > : {shift}",
+      ".com @ {space}",
+    ],
+  },
+};
