b7987fd0a38cf96af1f45ceaef11380538833cf7
Lotanna <47992836+odunze@users.noreply.github.com>
2023-04-04 11:41:50 -0400

    Update ndebe.ts
    Switched keyboard layout to have high vowels as default since they are commoner than mid vowels.

--- src/lib/layouts/ndebe.ts
+++ src/lib/layouts/ndebe.ts
@@ -7,18 +7,18 @@ import { LayoutItem } from "../interfaces";
  export default <LayoutItem>{
    layout: {
      default: [
-       "` \uE101 \uE102 \uE103 \uE104 \uE105 \uE106 \uE107 \uE108 \uE109 \uE100 - = {bksp}",
-       "{tab} \uE25A \uE300 \uE254 \uE351 \uE350 \uE301 \uE266 \uE354 \uE263 \uE255 [ ] \\",
-       "{lock} \uE257 \uE302 \uE353 \uE352 \uE303 \uE251 \uE355 \uE304 \uE269 ; ' {enter}",
-       "{shift} \uE356 \uE25D \uE305 \uE260 \uE26B \uE26C \uE26D , . / {shift}",
-       ".com @ {space}",
-     ],
-     shift: [
-       "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-       "{tab} \uE259 \uE10B \uE253 \uE10C \uE10D \uE10E \uE265 \uE10F \uE262 \uE25B { } |",
-       '\uE10A \uE256 \uE110 \uE111 \uE112 \uE113 \uE250 \uE267 \uE264 \uE268 : " {enter}',
-       "{shift} \uE258 \uE25C \uE252 \uE25F \uE26A \uE25E \uE261 < > ? {shift}",
-       ".com @ {space}",
-     ]
+      "` \uE101 \uE102 \uE103 \uE104 \uE105 \uE106 \uE107 \uE108 \uE109 \uE100 - = {bksp}",
+      "{tab} \uE259 \uE300 \uE253 \uE351 \uE350 \uE301 \uE265 \uE354 \uE262 \uE26B [ ] \\",
+      "{lock} \uE256 \uE302 \uE353 \uE352 \uE303 \uE250 \uE355 \uE304 \uE268 ; · {enter}",
+      "{shift} \uE356 \uE25C \uE305 \uE25F \uE254 \uE26C \uE26D , . / {shift}",
+      ".com @ {space}",
+    ],
+    shift: [
+      "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+      "{tab} \uE25B \uE10B \uE255 \uE10C \uE10D \uE10E \uE267 \uE10F \uE264 \uE25A { } |",
+      '\uE10A \uE258 \uE110 \uE111 \uE112 \uE113 \uE252 \uE266 \uE263 \uE26A : " {enter}',
+      "{shift} \uE257 \uE25E \uE251 \uE261 \uE269 \uE25D \uE260 < > ? {shift}",
+      ".com @ {space}",
+    ]
    },
  };
