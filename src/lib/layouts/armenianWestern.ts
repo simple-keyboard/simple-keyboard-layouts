0e876f7597def0eb46f630feacbb6a4dca99ff20
vidman22 <29338715+vidman22@users.noreply.github.com>
2023-06-15 17:00:08 +0200

    fix @ overwrite

--- src/lib/layouts/armenianWestern.ts
+++ src/lib/layouts/armenianWestern.ts
@@ -6,18 +6,18 @@ import { LayoutItem } from "../interfaces";
 export default <LayoutItem> {
 	layout: {
 		default: [
-            '՝ : ձ յ ՛ , - . « » օ ռ ժ {bksp}',
-            '{tab} խ վ է ր դ ե ը ի ո պ չ ջ',
-            "{lock} ա ս տ ֆ կ հ ճ ք լ թ փ ' {enter}",
-            '{shift} զ ց գ ւ բ ն մ շ ղ ծ {shift}',
-            '.com ձ {space}',
+          '՝ : ձ յ ՛ , - . « » օ ռ ժ {bksp}',
+          '{tab} խ վ է ր դ ե ը ի ո պ չ ջ',
+          "{lock} ա ս տ ֆ կ հ ճ ք լ թ փ ' {enter}",
+          '{shift} զ ց գ ւ բ ն մ շ ղ ծ {shift}',
+          '.com @ {space}',
 		],
 		shift: [
-            '՜ 1 Ձ Յ 3 4 9 և ( ) Օ Ռ Ժ {bksp}',
-            '{tab} Խ Վ Է Ր Դ Ե Ը Ի Ո Պ Չ Ջ',
-            '{lock} Ա Ս Տ Ֆ Կ Հ Ճ Ք Լ Թ Փ ՞ {enter}',
-            '{shift} Զ Ց Գ Ւ Բ Ն Մ Շ Ղ Ծ {shift}',
-            '.com Ձ {space}',
+          '՜ 1 Ձ Յ 3 4 9 և ( ) Օ Ռ Ժ {bksp}',
+          '{tab} Խ Վ Է Ր Դ Ե Ը Ի Ո Պ Չ Ջ',
+          '{lock} Ա Ս Տ Ֆ Կ Հ Ճ Ք Լ Թ Փ ՞ {enter}',
+          '{shift} Զ Ց Գ Ւ Բ Ն Մ Շ Ղ Ծ {shift}',
+          '.com @ {space}',
 		],
 	},
 };
