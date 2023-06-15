3f58d7ca6a29ad069e2eaaf678355adbca5d1651
vidman22 <29338715+vidman22@users.noreply.github.com>
2023-06-15 15:09:41 +0200

    add Armenian keyboards

---
+++ src/lib/layouts/armenianEastern.ts
@@ -0,0 +1,23 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: Armenian Eastern
+ */
+export default <LayoutItem> {
+	layout: {
+		default: [
+			'՝ : ձ յ ՛ , - . « » օ ռ ժ {bksp}',
+			'{tab} խ ւ է ր տ ե ը ի ո պ չ ջ',
+			"{lock} ա ս դ ֆ ք հ ճ կ լ թ փ ' {enter}",
+			'{shift} զ ց գ վ բ ն մ շ ղ ծ {shift}',
+			'.com ձ {space}',
+		],
+		shift: [
+			'՜ 1 Ձ Յ 3 4 9 և ( ) Օ Ռ Ժ {bksp}',
+			'{tab} Խ Ւ Է Ր Տ Ե Ը Ի Ո Պ Չ Ջ',
+			'{lock} Ա Ս Դ Ֆ Ք Հ Ճ Կ Լ Թ Փ ՞ {enter}',
+			'{shift} Զ Ց Գ Վ Բ Ն Մ Շ Ղ Ծ {shift}',
+			'.com Ձ {space}',
+		],
+	},
+};
