d57370db7c4f8b31e0d729253a1da3b9926640f2
Francisco Hodge <hello@franciscohodge.com>
2024-02-19 09:16:46 +0000

    Build update

--- build/index.esm.js
+++ build/index.esm.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-layouts v3.3.71 (ESM)
+ *   simple-keyboard-layouts v3.3.72 (ESM)
  *   https://github.com/hodgef/simple-keyboard-layouts
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
